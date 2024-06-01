4798f4331f1811d4083311c3510bce185b84e6e5
Alehandro <honestas@mail.ru>
2024-06-02 04:49:49 +0700

    homework Classes and objects

---
+++ homework 3.1/homework 3.1/homework 3.1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+
+class Calculator
+{
+private:
+	double num1,
+		   num2;
+public:
+	Calculator()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+
+	void request()
+	{
+		std::cout << "Введите num1: " << std::endl;
+		std::cin >> num1;
+		std::cout << "Введите num2: " << std::endl;
+		std::cin >> num2;
+	}
+
+	void actions()
+	{
+		std::cout << "Сложение:.................." << num1 + num2 << std::endl;
+		std::cout << "Вычитание num2 из num1:...." << num1 - num2 << std::endl;
+		std::cout << "Вычитание num1 из num2:...." << num2 - num1 << std::endl;
+		std::cout << "Умножение:................." << num1 * num2 << std::endl;
+		if (num1 != 0)
+		{
+			std::cout << "Деление num2 на num1:......" << num2 / num1 << std::endl;
+		}
+		else
+		{
+			std::cout << "<<< Соблюдайте правила арифметики!!! На ноль делить нельзя!!! >>>" << std::endl;
+		}
+		if (num2 != 0)
+		{
+			std::cout << "Деление num1 на num2:......" << num1 / num2 << std::endl;
+		}
+		else
+		{
+			std::cout << "<<< Соблюдайте правила арифметики!!! На ноль делить нельзя!!! >>>" << std::endl;
+		}
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	char choice;
+
+	Calculator callCalc;
+
+	for ( ; ; )
+	{
+		callCalc.request();
+		callCalc.actions();
+
+		std::cout << "Хотите продолжить? (y / n): " << std::endl;
+		std::cin >> choice;
+		if (choice != 'y' && choice != 'Y')
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
---
+++ homework 3.2/Homework 3.2/Homework 3.2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+
+class Counter
+{
+private:
+	int count;
+
+public:
+	Counter() : count(0) {};
+
+	void initV (int val)
+	{
+		count = val;
+	}
+
+	void inc()
+	{
+		count++;
+	}
+
+	void dec()
+	{
+		--count;
+	}
+
+	int equals ()
+	{
+		return count;
+	}
+};
+
+
+
+int main()
+{
+	setlocale (LC_ALL, "Russian");
+
+	Counter counter;
+
+	int mng;
+	char choice;
+
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да (y) или нет (n): ";
+	std::cin >> choice;
+
+	if (choice == 'y')
+	{
+		std::cout << "Введите начальное значение счётчика: ";
+		std::cin >> mng;
+		counter.initV(mng);
+	}
+
+	for (; ; )
+	{
+		std::cout << "Введите команду ( '+', '-', '=' или 'x'): ";
+		std::cin >> choice;
+
+		if (choice == '+')
+		{
+			counter.inc();
+		}
+		if (choice == '-')
+		{
+			counter.dec();
+		}
+		if (choice == '=')
+		{
+			std::cout << counter.equals() << std::endl;
+		}
+		if (choice == 'x')
+		{
+			std::cout << "До свидания!" << std::endl;
+			break;
+		}
+	}
+
+	return 0;
+}

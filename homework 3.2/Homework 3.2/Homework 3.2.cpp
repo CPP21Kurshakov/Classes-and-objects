f7f8facacdd0b7bb6e512f1001c8f8777cc0570b
Alehandro <honestas@mail.ru>
2024-06-02 16:37:56 +0700

    Corrected code 3

--- homework 3.2/Homework 3.2/Homework 3.2.cpp
+++ homework 3.2/Homework 3.2/Homework 3.2.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <string>
+#include <Windows.h>
 
 class Counter
 {
@@ -35,17 +36,17 @@ public:
 int main()
 {
 	setlocale (LC_ALL, "Russian");
+	SetConsoleCP(1251);
 
 	Counter counter;
 
 	int mng;
-	char choice;
-	char choice2;
+	std::string choice;
 
 	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-	std::cin >> choice >> choice2;
+	std::cin >> choice;
 
-	if (choice == (int) choice && choice2 == (int) choice2)
+	if (choice == "да")
 	{
 		std::cout << "Введите начальное значение счётчика: ";
 		std::cin >> mng;
@@ -57,19 +58,19 @@ int main()
 		std::cout << "Введите команду ( '+', '-', '=' или 'x'): ";
 		std::cin >> choice;
 
-		if (choice == '+')
+		if (choice == "+")
 		{
 			counter.inc();
 		}
-		if (choice == '-')
+		if (choice == "-")
 		{
 			counter.dec();
 		}
-		if (choice == '=')
+		if (choice == "=")
 		{
 			std::cout << counter.equals() << std::endl;
 		}
-		if (choice == 'x')
+		if (choice == "x")
 		{
 			std::cout << "До свидания!" << std::endl;
 			break;
